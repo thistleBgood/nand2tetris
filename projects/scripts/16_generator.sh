ca9f6ffa85bbbd35f6f06d5a19479835b4153dca
David Creigh <thistleBgood@users.noreply.github.com>
2017-12-29 23:35:05 +0000

    Implemented Register chip, using generator script.

--- projects/scripts/16_generator.sh
+++ projects/scripts/16_generator.sh
@@ -1,6 +1,6 @@
 max=15
 for i in `seq 0 $max`
 do
-     echo "    FullAdder (a=a[$i], b=b[$i], c=carry$i, sum=out[$i], carry=carry$((i+1)));"
+     echo "    Bit (in=in[$i], load=load, out=out[$i]);"
 done
 
